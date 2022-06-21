db5293b55e36f1194bcfb090d97df6dd95308369
Humberto-Bonadiman <humberto_bonadiman@hotmail.com>
2022-06-21 18:41:18 -0400

    feat: testes transacao completos

--- src/test/java/com/trybe/acc/java/caixaeletronico/TransacaoTest.java
+++ src/test/java/com/trybe/acc/java/caixaeletronico/TransacaoTest.java
@@ -1,43 +1,57 @@
 package com.trybe.acc.java.caixaeletronico;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 @DisplayName("Testes dos métodos da classe Transacao")
 class TransacaoTest {
+  private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+  private final PrintStream originalOut = System.out;
 
+  @BeforeEach
+  public void setUpStreams() {
+    System.setOut(new PrintStream(outContent));
+  }
+
+  @AfterEach
+  public void restoreStreams() {
+    System.setOut(originalOut);
+  }
 
   @Test
   @DisplayName("21 - Testa o método construtor da classe Transacao.")
   void construtorTest() {
     Transacao transacao = new Transacao(500, "deposito");
     assertEquals("deposito", transacao.getDescricao());
-    String quantia = String.valueOf(transacao.getQuantia());
-    assertEquals(quantia, transacao.getQuantia());
+    assertEquals(500, transacao.getQuantia());
   }
-
 
   @Test
   @DisplayName("22 - Testa o método Getter do atributo quantia.")
   void getQuantiaTest() {
-    fail("Não implementado");
-
+    Transacao transacao = new Transacao(500, "deposito");
+    assertEquals(500, transacao.getQuantia());
   }
 
   @Test
   @DisplayName("23 - Testa o método retornar resumo transacao.")
   void retornarResumoTransacaoTest() {
-    fail("Não implementado");
-
+    Transacao transacao = new Transacao(500, "deposito");
+    assertTrue(transacao.retornarResumoTransacao().contains(transacao.retornarResumoTransacao()));
   }
 
   @Test
   @DisplayName("24 - Testa o método instante está gerando o instante corretamente.")
   void retornarInstanteTest() {
-    fail("Não implementado");
-
+    Transacao transacao = new Transacao(500, "deposito");
+    assertNotNull(transacao.retornarInstante());
   }
 
 }
